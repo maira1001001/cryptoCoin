97b089777a627fd26685d7cd4fde0feb218af2cf
Maira Diaz <maira.marraffini@gmail.com>
2022-05-16 00:30:46 +0300

    add constants

---
+++ constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    provider: "ws://localhost:8545",
+    id: "4",
+    contractAddress: "0x14143f048589e3ed28BA550a2e54557549B39C92",
+    account2: "0xe50481D551c6B1Db416DCE0afCb891022AFd3d88"
+}
